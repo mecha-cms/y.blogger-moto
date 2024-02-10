a4be2370f4b6e52e0e0905b55cc8ed6a180b25fc
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-02-10 12:03:12 +0700

    HTML `<search>`

--- widget/form/search.php
+++ widget/form/search.php
@@ -1,6 +1,6 @@
 <?php if (isset($state->x->search)): ?>
   <?= self::widget([
-      'content' => $content ?? self::form('search', ['route' => $route ?? $state->routeBlog ?? '/article']),
+      'content' => '<search>' . ($content ?? self::form('search', ['route' => $route ?? $state->routeBlog ?? '/article'])) . '</search>',
       'title' => $title ?? i('Search')
   ]); ?>
 <?php endif; ?>
